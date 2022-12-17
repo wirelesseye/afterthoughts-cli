625bf7e0b7cec7e189e8f571928e8fb1b7565a58
wirelesseye <okuloj@outlook.com>
2022-12-17 20:21:38 +1100

    add vite-tsconfig-paths

--- vite.prerender.config.ts
+++ vite.prerender.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,5 +31,5 @@ export default defineConfig({
             "import.meta.url": "import.meta.url",
         },
     },
-    plugins: [react()],
+    plugins: [tsconfigPaths(), react()],
 });
